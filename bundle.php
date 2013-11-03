5a1f2397d1e7af4e1e34a9b861b02649cd7a0901
unknown <Michel>
2013-11-03 20:05:26 +1100

    libification

--- bundle.php
+++ bundle.php
@@ -1,4 +1,15 @@
 <?php
-\Coxis\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => 'FAQ', 'link' => 'faq');
+namespace App\Faq;
 
-\Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('faq/icon.svg'), 'link'=>'faq', 'title' => __('FAQ'), 'description' => __('All the questions'));
+class Bundle extends \Coxis\Core\BundleLoader {
+	public function load($queue) {
+		parent::load();
+	}
+
+	public function run() {
+		\App\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => 'FAQ', 'link' => 'faq');
+		\App\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('faq/icon.svg'), 'link'=>'faq', 'title' => __('FAQ'), 'description' => __('All the questions'));
+		parent::run();
+	}
+}
+return new Bundle;
