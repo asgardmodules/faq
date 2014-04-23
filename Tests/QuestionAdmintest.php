314608d940185bb74ad432ba93c7ae7156a3bb99
Michel Hognerud <leyou.m@gmail.com>
2014-04-24 07:22:00 +0800

    fixes

--- Tests/QuestionAdmintest.php
+++ Tests/QuestionAdmintest.php
@@ -7,7 +7,7 @@ Asgard::load();
 class QuestionAdminTest extends PHPUnit_Framework_TestCase {
 	public function setUp(){
 		\Schema::dropAll();
-		ORMManager::autobuild();
+		MigrationsManager::autobuild();
 		\BundlesManager::loadEntityFixturesAll();
 	}
 	public function tearDown(){}
