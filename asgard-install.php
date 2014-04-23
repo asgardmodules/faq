4cd68698220fca882d2309b7fbabf99bd92b7e06
Michel Hognerud <leyou.m@gmail.com>
2014-04-23 18:01:03 +0800

    fixing asgard-install

--- asgard-install.php
+++ asgard-install.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__.'/../utils/FileManager.php';
+require_once __DIR__.'/../utils/FileManager.php';
 
 \Asgard\Utils\FileManager::copy(__DIR__.'/app/faq', 'app/faq');
 \Asgard\Utils\FileManager::copy(__DIR__.'/tests/QuestionAdmintest.php', 'tests/QuestionAdmintest.php');
