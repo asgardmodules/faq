1524680b6e8f149b66d175eabe91c40cf5937670
Michel Hognerud <leyou.m@gmail.com>
2014-04-24 07:00:38 +0800

    migrations

--- asgard-install.php
+++ asgard-install.php
@@ -1,10 +1,11 @@
 <?php
-require_once __DIR__.'/../utils/FileManager.php';
-require_once __DIR__.'/../orm/Libs/MigrationsManager.php';
+require_once 'paths.php';
+require _CORE_DIR_.'core.php';
+\Asgard\Core\App::loadDefaultApp();
 
-\Asgard\Utils\FileManager::copy(__DIR__.'/app/faq', 'app/faq');
-\Asgard\Utils\FileManager::copy(__DIR__.'/tests/QuestionAdmintest.php', 'tests/QuestionAdmintest.php');
-\Asgard\Utils\FileManager::copy(__DIR__.'/web/faq', 'web/faq');
+\Asgard\Utils\FileManager::copy(__DIR__.'/app/faq', _DIR_.'app/faq');
+\Asgard\Utils\FileManager::copy(__DIR__.'/tests/QuestionAdmintest.php', _DIR_._DIR_.'tests/QuestionAdmintest.php');
+\Asgard\Utils\FileManager::copy(__DIR__.'/web/faq', _DIR_.'web/faq');
 
 \Asgard\Orm\Libs\MigrationsManager::addMigrationFile('migrations/Faq.php');
 \Asgard\Orm\Libs\MigrationsManager::migrate('Faq');
